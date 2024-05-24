23431d399e677400148598039bb0ef3a4ecff809
vishakha-dhumal <159988097+vishakha-dhumal@users.noreply.github.com>
2024-05-24 15:22:21 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -22,34 +22,72 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package com.iluwatar.acyclicvisitor;
+package com.iluwatar.roleobject;
+
+import static com.iluwatar.roleobject.Role.BORROWER;
+import static com.iluwatar.roleobject.Role.INVESTOR;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
- * The Acyclic Visitor pattern allows new functions to be added to existing class hierarchies
- * without affecting those hierarchies, and without creating the dependency cycles that are inherent
- * to the GoF Visitor pattern, by making the Visitor base class degenerate
+ * The Role Object pattern suggests to model context-specific views of an object as separate role
+ * objects which are dynamically attached to and removed from the core object. We call the resulting
+ * composite object structure, consisting of the core and its role objects, a subject. A subject
+ * often plays several roles and the same role is likely to be played by different subjects. As an
+ * example consider two different customers playing the role of borrower and investor, respectively.
+ * Both roles could as well be played by a single {@link Customer} object. The common superclass for
+ * customer-specific roles is provided by {@link CustomerRole}, which also supports the {@link
+ * Customer} interface.
  *
- * <p>In this example the visitor base class is {@link ModemVisitor}. The base class of the visited
- * hierarchy is {@link Modem} and has two children {@link Hayes} and {@link Zoom} each one having
- * its own visitor interface {@link HayesVisitor} and {@link ZoomVisitor} respectively. {@link
- * ConfigureForUnixVisitor} and {@link ConfigureForDosVisitor} implement each derivative's visit
- * method only if it is required
+ * <p>The {@link CustomerRole} class is abstract and not meant to be instantiated.
+ * Concrete subclasses of {@link CustomerRole}, for example {@link BorrowerRole} or {@link
+ * InvestorRole}, define and implement the interface for specific roles. It is only these subclasses
+ * which are instantiated at runtime. The {@link BorrowerRole}  class defines the context-specific
+ * view of {@link Customer} objects as needed by the loan department. It defines additional
+ * operations to manage the customer’s credits and securities. Similarly, the {@link InvestorRole}
+ * class adds operations specific to the investment department’s view of customers. A client like
+ * the loan application may either work with objects of the {@link CustomerRole} class, using the
+ * interface class {@link Customer}, or with objects of concrete {@link CustomerRole} subclasses.
+ * Suppose the loan application knows a particular {@link Customer} instance through its {@link
+ * Customer} interface. The loan application may want to check whether the {@link Customer} object
+ * plays the role of Borrower. To this end it calls {@link Customer#hasRole(Role)} with a suitable
+ * role specification. For the purpose of our example, let’s assume we can name roles with enum. If
+ * the {@link Customer} object can play the role named “Borrower,” the loan application will ask it
+ * to return a reference to the corresponding object. The loan application may now use this
+ * reference to call Borrower-specific operations.
  */
-public class App {
+@Slf4j
+public class ApplicationRoleObject {
 
   /**
-   * Program's entry point.
+   * Main entry point.
+   *
+   * @param args program arguments
    */
   public static void main(String[] args) {
-    var conUnix = new ConfigureForUnixVisitor();
-    var conDos = new ConfigureForDosVisitor();
+    var customer = Customer.newCustomer(BORROWER, INVESTOR);
 
-    var zoom = new Zoom();
-    var hayes = new Hayes();
+    LOGGER.info(" the new customer created : {}", customer);
 
-    hayes.accept(conDos); // Hayes modem with Dos configurator
-    zoom.accept(conDos); // Zoom modem with Dos configurator
-    hayes.accept(conUnix); // Hayes modem with Unix configurator
-    zoom.accept(conUnix); // Zoom modem with Unix configurator   
+    var hasBorrowerRole = customer.hasRole(BORROWER);
+    LOGGER.info(" customer has a borrowed role - {}", hasBorrowerRole);
+    var hasInvestorRole = customer.hasRole(INVESTOR);
+    LOGGER.info(" customer has an investor role - {}", hasInvestorRole);
+
+    customer.getRole(INVESTOR, InvestorRole.class)
+        .ifPresent(inv -> {
+          inv.setAmountToInvest(1000);
+          inv.setName("Billy");
+        });
+    customer.getRole(BORROWER, BorrowerRole.class)
+        .ifPresent(inv -> inv.setName("Johny"));
+
+    customer.getRole(INVESTOR, InvestorRole.class)
+        .map(InvestorRole::invest)
+        .ifPresent(LOGGER::info);
+
+    customer.getRole(BORROWER, BorrowerRole.class)
+        .map(BorrowerRole::borrow)
+        .ifPresent(LOGGER::info);
   }
 }
